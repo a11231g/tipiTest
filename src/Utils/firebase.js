954d3097eb6e0a8bd621b4c0bcabc56678a1e370
ali atik <ali1wg@gmail.com>
2019-05-19 23:41:41 +0430

    last push

--- src/Utils/firebase.js
+++
@@ -1,7 +0,0 @@
-import firebase from "react-native-firebase";
-
-export async function getFireBasetoken() {
-  const fb = firebase.messaging();
-  fb.requestPermission();
-  return await fb.getToken().then(token => token);
-}
--- src/Utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function isEmpty(obj) {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) return false;
-  }
-  return true;
-}
-export function Capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
